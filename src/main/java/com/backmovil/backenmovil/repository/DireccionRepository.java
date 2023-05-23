92ac86812b016f1e5544263d1bf9f7e2388fc18e
Juan Sebastian Lizcano <juansebasliz01@hotmail.com>
2023-05-22 22:18:12 -0500

    Correcion de DireccionRepository

--- src/main/java/com/backmovil/backenmovil/repository/DireccionRepository.java
+++ src/main/java/com/backmovil/backenmovil/repository/DireccionRepository.java
@@ -1,8 +1,9 @@
 package com.backmovil.backenmovil.repository;
 
+import com.backmovil.backenmovil.entity.DireccionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DireccionRepository extends JpaRepository<DireccionRepository, Long> {
+public interface DireccionRepository extends JpaRepository<DireccionEntity, Long> {
 }
