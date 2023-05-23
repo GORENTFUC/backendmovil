a1faaada04ec0ab1b41c4aa4230cc507ad9807cb
Juan Sebastian Lizcano <ingsosorio77@gmail.com>
2023-05-22 20:54:18 -0500

    Creacion de la Entidad Usuario

---
+++ src/main/java/com/backmovil/backenmovil/BackenmovilApplication.java
@@ -0,0 +1,13 @@
+package com.backmovil.backenmovil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenmovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenmovilApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/backmovil/backenmovil/entity/UsuarioEntity.java
@@ -0,0 +1,32 @@
+package com.backmovil.backenmovil.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idusuario")
+    private Long idUsuario;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "correoelectronico")
+    private String correo;
+
+    @Column(name = "contrasena")
+    private String contrasena;
+
+    @Column(name = "celular")
+    private String celular;
+
+
+
+}
