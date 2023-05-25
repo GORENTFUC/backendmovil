69ec312cc573407ab42f39651f2f1447d6d1fc10
Juan Sebastian Lizcano <juansebasliz01@hotmail.com>
2023-05-25 11:17:39 -0500

    se crea dto de Bebida

---
+++ src/main/java/com/backmovil/backenmovil/dto/BebidaDto.java
@@ -0,0 +1,13 @@
+package com.backmovil.backenmovil.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BebidaDto {
+
+
+    private String nombre;
+    private int cantidad;
+}
